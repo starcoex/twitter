08c55125961b64d966fea1303a791152fa4daa81
starcoex <69073390+starcoex@users.noreply.github.com>
2022-11-13 22:37:07 +0900

    1.0.1 firebase Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import app from "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
